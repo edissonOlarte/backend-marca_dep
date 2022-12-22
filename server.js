f290134ead97cdff43ee2ba0b4daf10c6b897513
edissonOlarte <edisson850609@hotmail.com>
2022-12-22 17:43:38 -0500

    first comit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
-
+require('dotenv').config()
 const express=require('express')
-const mysql=require('mysql')
+const mysql=require('mysql2')
 const myconn=require('express-myconnection')
 const routes=require('./routes')
 const cors = require('cors')
@@ -9,14 +9,21 @@ const cors = require('cors')
 const app=express()
 app.use(cors())
 
-app.set('port',9000)
+app.set('port',process.env.PORT_SERVER  || 9000)
 
 const dbOptions={
+    host: process.env.DB_HOST || 'localhost',
+    port:process.env.DB_PORT || '3306',
+    user: process.env.BD_USER || 'marcador',
+    password: process.env.DB_PWD ||'edi-850609',
+    database: process.env.DB_NAME ||'libreria'
+
+/*const dbOptions={
     host: 'localhost',
     port: '3306',
     user: 'marcador',
     password: 'edi-850609',
-    database: 'libreria'
+    database: 'libreria'*/
 }
 
 //middleware
